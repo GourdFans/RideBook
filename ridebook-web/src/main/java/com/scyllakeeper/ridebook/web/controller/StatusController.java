e113955960b752212f3b85ab692076bed67e7e2c
桌子 <zhuozi@mogujie.com>
2017-05-26 10:19:21 +0800

    1. 搭建项目基本框架 api 提供对外接口 impl 具体实现 biz  具体业务实现 utils 常用工具类 web  http接口

---
+++ ridebook-web/src/main/java/com/scyllakeeper/ridebook/web/controller/StatusController.java
@@ -0,0 +1,23 @@
+package com.scyllakeeper.ridebook.web.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by zhuozi on 17/5/21.
+ */
+@Controller
+@RequestMapping("/status")
+public class StatusController {
+
+    @ResponseBody
+    @RequestMapping(value = "")
+    public JSONObject status(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", 1001);
+        jsonObject.put("message", "success");
+        return jsonObject;
+    }
+}
