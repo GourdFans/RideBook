05ec0292e894562c8340132d5a4ab7308c7b4d95
桌子 <zhuozi@mogujie.com>
2017-05-27 11:42:28 +0800

    前后端还是分离开，添加日志

--- ridebook-web/src/main/java/com/scyllakeeper/ridebook/web/controller/StatusController.java
+++ ridebook-web/src/main/java/com/scyllakeeper/ridebook/web/controller/StatusController.java
@@ -1,6 +1,8 @@
 package com.scyllakeeper.ridebook.web.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("")
 public class StatusController {
+    Logger logger = LoggerFactory.getLogger(StatusController.class);
 
     @ResponseBody
     @RequestMapping(value = "/status")
-    public JSONObject status(){
+    public String status(){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("code", 1001);
         jsonObject.put("message", "success");
-        return jsonObject;
+        logger.debug(jsonObject.toJSONString());
+        logger.info(jsonObject.toJSONString());
+        logger.warn(jsonObject.toJSONString());
+        logger.error(jsonObject.toJSONString());
+        return jsonObject.toJSONString();
     }
 }
